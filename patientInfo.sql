d74fdf2e91b394fc437039330111a73f19c8a64a
root <root@localhost.localdomain>
2017-03-30 16:26:23 +0000

    all intermediary tables created

--- patientInfo.sql
+++ patientInfo.sql
@@ -7,7 +7,7 @@ CREATE TABLE patient(
 	patient_id varchar(5) not null,
 	fname varchar(20),
 	lname varchar(20),
-	address varchar(20),
+	address varchar(30),
 	dob date,
 	phone varchar(8),
 	primary key(patient_id) 
@@ -56,7 +56,7 @@ CREATE TABLE diagnosis(
 );
 
 CREATE TABLE procedure(
-	prodecure_id varchar(5) not null,
+	procedure_id varchar(5) not null,
 	name varchar(50),
 	primary key(procedure_id)
 );
@@ -81,4 +81,116 @@ CREATE TABLE result(
 	primary key(result_id)
 );
 
+CREATE TABLE medical_rec(
+	record_id varchar(5),
+	primary key(record_id)
+);
 
+
+/* INTERMEDIARY TABLES */
+
+CREATE TABLE DPAttends(
+	doctor_id varchar(5),
+	patient_id varchar(5),
+	FOREIGN KEY doctor_id references doctor(doctor_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY patient_id references patient(patient_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE NPAttends(
+	nurse_id varchar(5),
+	patient_id varchar(5),
+	FOREIGN KEY nurse_id references nurse(nurse_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY patient_id references patient(patient_id) ON DELETE CASCADE ON UPDATE CASCADE
+
+);
+
+CREATE TABLE patientMedical(
+	record_id varchar(5),
+	patient_id varchar(5),
+	FOREIGN KEY patient_id references patient(patient_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY record_id REFERENCES medical_rec(record_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE patientRelation(
+	patient_id varchar(5),
+	FOREIGN KEY patient_id references patient(patient_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE retrieves(
+	nurse_id varchar(5),
+	record_id varchar(5),
+	FOREIGN KEY nurse_id REFERENCES nurse(nurse_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY record_id REFERENCES medical_rec(record_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE administers(
+	nurse_id varchar(5),
+	med_id varchar(5),
+	FOREIGN KEY nurse_id REFERENCES nurse(nurse_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY med_id REFERENCES medicine(med_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE medicineRecorded(
+	med_id varchar(5),
+	record_id varchar(5),
+	FOREIGN KEY med_id REFERENCES medicine(med_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY record_id REFERENCES medical_rec(record_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE updateVital(
+	vital_id varchar(5),
+	nurse_id varchar(5),
+	FOREIGN KEY vital_id REFERENCES vitals(vital_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY nurse_id REFERENCES nurse(nurse_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE vitalRecorded(
+	vital_id varchar(5),
+	record_id varchar(5),
+	FOREIGN KEY vital_id REFERENCES vitals(vital_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY record_id REFERENCES medical_rec(record_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE docManage(
+	doctor_id varchar(5),
+	record_id varchar(5),
+	FOREIGN KEY doctor_id REFERENCES doctor(doctor_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY record_id REFERENCES medical_rec(record_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE diagRecorded(
+	diagnosis_id varchar(5),
+	record_id varchar(5),
+	FOREIGN KEY diagnosis_id REFERENCES diagnosis(diagnosis_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY record_id REFERENCES medical_rec(record_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE procedureRecorded(
+	procedure_id varchar(5),
+	record_id varchar(5),
+	FOREIGN KEY procedure_id REFERENCES procedure(procedure_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY record_id REFERENCES medical_rec(record_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE procedureStep(
+	procedure_id varchar(5),
+	step_id varchar(5),
+	FOREIGN KEY(procedure_id) REFERENCES procedure(procedure_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(step_id) REFERENCES step(step_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE treatmentRecorded(
+	treatment_id varchar(5),
+	record_id varchar(5),
+	FOREIGN KEY(treatment_id) REFERENCES treatment(treatment_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(record_id) REFERENCES medical_rec(record_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE resultRecorded(
+	result_id varchar(5),
+	record_id varchar(5),
+	FOREIGN KEY(result_id) REFERENCES result(result_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(record_id) REFERENCES medical_rec(record_id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+/* ALL ISA TABLES*/
